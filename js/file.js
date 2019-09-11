7224da447f23b0a7a8e5ea668aa3e9cd267b81b7
mohamed <33775327+benessaessa@users.noreply.github.com>
2019-09-11 10:35:13 +0200

    edit

---
+++ js/file.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#showImage").click(function(){
+      $('#imageHide').css("display","block");
+      });againHide
+      $("#againHide").click(function(){
+        $("#imageHide").css("display","none");
+      });
+  });
